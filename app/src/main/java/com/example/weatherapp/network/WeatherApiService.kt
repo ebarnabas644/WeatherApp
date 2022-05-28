5e2879d4fac2d0a717c483a24e1ac3c380ed5cbe
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-05-28 15:28:32 +0200

    Creating api service

--- app/src/main/java/com/example/weatherapp/network/WeatherApiService.kt
+++ app/src/main/java/com/example/weatherapp/network/WeatherApiService.kt
@@ -1,5 +1,31 @@
 package com.example.weatherapp.network
 
-class WeatherApiService {
+import com.example.weatherapp.network.dataclass.Weather
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
 
+private const val BASE_URL =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+private const val QUERY_DATA = "?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Caddress%2CresolvedAddress%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Chumidity%2Cwindspeed%2Cwinddir%2Cuvindex%2Csunrise%2Csunset%2Cdescription%2Cicon&key=2YM68T2QCXTW94ESQVKNGJ4CJ&contentType=json"
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface WeatherApiService {
+    @GET("VisualCrossingWebServices/rest/services/timeline/{address}$QUERY_DATA")
+    suspend fun getWeather(@Path("address") address : String): Weather
+}
+
+object WeatherApi {
+    val retrofitService : WeatherApiService by lazy {
+        retrofit.create(WeatherApiService::class.java)
+    }
 }
