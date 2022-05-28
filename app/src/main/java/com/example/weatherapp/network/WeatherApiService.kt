de370ec5157600cb812c50125e257f383f5de73c
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-05-28 20:37:51 +0200

    Bad URL path fixed

--- app/src/main/java/com/example/weatherapp/network/WeatherApiService.kt
+++ app/src/main/java/com/example/weatherapp/network/WeatherApiService.kt
@@ -3,6 +3,8 @@ package com.example.weatherapp.network
 import com.example.weatherapp.network.dataclass.Weather
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -14,13 +16,14 @@ private const val QUERY_DATA = "?unitGroup=metric&elements=datetime%2CdatetimeEp
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
+
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface WeatherApiService {
-    @GET("VisualCrossingWebServices/rest/services/timeline/{address}$QUERY_DATA")
+    @GET("{address}$QUERY_DATA")
     suspend fun getWeather(@Path("address") address : String): Weather
 }
 
