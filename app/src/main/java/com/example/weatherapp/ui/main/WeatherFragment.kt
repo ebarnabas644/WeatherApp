9c478897d9287f396b3cc985c8ca38c17afe4993
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-05-28 20:32:37 +0200

    Setting up view model binding

---
+++ app/src/main/java/com/example/weatherapp/ui/main/WeatherFragment.kt
@@ -0,0 +1,31 @@
+package com.example.weatherapp.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.weatherapp.databinding.FragmentMainBinding
+
+class WeatherFragment : Fragment() {
+
+    private val viewModel: WeatherViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentMainBinding.inflate(inflater)
+        //val binding = GridViewItemBinding.inflate(inflater)
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = this
+
+        // Giving the binding access to the OverviewViewModel
+        binding.viewModel = viewModel
+        return binding.root
+    }
+
+}
