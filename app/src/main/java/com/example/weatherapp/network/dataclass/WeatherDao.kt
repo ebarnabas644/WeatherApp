e88fd69eb6f8fc486d7b2f22ce0eab7f24138027
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-06-29 21:32:39 +0200

    Data from database returning as Flow

--- app/src/main/java/com/example/weatherapp/network/dataclass/WeatherDao.kt
+++ app/src/main/java/com/example/weatherapp/network/dataclass/WeatherDao.kt
@@ -2,7 +2,9 @@ package com.example.weatherapp.network.dataclass
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 
 //https://www.answertopia.com/jetpack-compose/a-jetpack-compose-room-database-and-repository-tutorial/
 @Dao
@@ -11,14 +13,17 @@ interface WeatherDao {
     suspend fun insert(item: Weather)
 
     @Update
-    suspend fun update(item: Weather)
+    fun update(item: Weather)
 
-    @Delete
-    suspend fun delete(item: Weather)
+    @Query("DELETE FROM Weather WHERE address = :address")
+    suspend fun delete(address: String)
 
     @Query("SELECT * FROM Weather WHERE address = :address LIMIT 1")
     suspend fun findByAddress(address: String): Weather
 
     @Query("SELECT * from Weather ORDER BY address ASC")
-    suspend fun getItems(): List<Weather>
+    fun getItems(): Flow<List<Weather>>
+
+    @Query("SELECT address from Weather ORDER BY address ASC")
+    fun getAddresses(): Flow<List<String>>
 }
