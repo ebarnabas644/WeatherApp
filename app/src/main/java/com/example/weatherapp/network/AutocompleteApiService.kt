c7b59329d433c0c07e56a111213b7c067f9a7a37
ebarnabas <ebarnabas@stud.uni-obuda.hu>
2022-07-02 14:40:30 +0200

    Api keys removed

--- app/src/main/java/com/example/weatherapp/network/AutocompleteApiService.kt
+++ app/src/main/java/com/example/weatherapp/network/AutocompleteApiService.kt
@@ -1,19 +1,16 @@
 package com.example.weatherapp.network
 
 import com.example.weatherapp.network.dataclass.Predictions
-import com.example.weatherapp.network.dataclass.Weather
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 private const val BASE_URL = "https://maps.googleapis.com/maps/api/place/autocomplete/"
-private const val QUERY_DATA = "json?types=geocode&key=AIzaSyCNMRHO8hSI88ZpBkTa1zPqv30V_5EZT94"
+private const val QUERY_DATA = "json?types=geocode"
+private const val API_KEY = "&key="  //Google autocomplete api key here
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
@@ -24,7 +21,7 @@ private val retrofit = Retrofit.Builder()
     .build()
 
 interface AutocompleteApiService {
-    @GET("$QUERY_DATA")
+    @GET("$QUERY_DATA$API_KEY")
     suspend fun getAutocomplete(@Query("input") address : String): Predictions
 }
 
